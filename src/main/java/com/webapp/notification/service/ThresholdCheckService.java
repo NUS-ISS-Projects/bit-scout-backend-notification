e344c09d1e1acadfd9bbef78f8b239babdb681b7
ngtianxun08 <e1327862@u.nus.edu>
2024-09-16 17:34:40 +0800

    * Migrate to firestore instead of jpa.

--- src/main/java/com/webapp/notification/service/ThresholdCheckService.java
+++ src/main/java/com/webapp/notification/service/ThresholdCheckService.java
@@ -1,27 +1,42 @@
 package com.webapp.notification.service;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
 import com.webapp.notification.dto.NotificationDto;
 import com.webapp.notification.dto.PriceUpdateDto;
 import com.webapp.notification.entity.Notification;
-import com.webapp.notification.repository.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 @Service
 public class ThresholdCheckService {
 
     @Autowired
-    private NotificationRepository notificationRepository;
+    private Firestore firestore;
 
     @Autowired
     private NotificationService notificationService;
 
+    private static final String COLLECTION_NAME = "notifications";
+
     // Accept price updates from Kafka and check thresholds for each user
-    public void checkThresholdsForAllUsers(PriceUpdateDto priceUpdate) {
-        List<Notification> notifications = notificationRepository.findByToken(priceUpdate.getToken());
-        
+    public void checkThresholdsForAllUsers(PriceUpdateDto priceUpdate) throws InterruptedException, ExecutionException {
+        // Query Firestore for notifications by token (equivalent to findByToken in JPA)
+        CollectionReference notificationsCollection = firestore.collection(COLLECTION_NAME);
+        ApiFuture<QuerySnapshot> future = notificationsCollection
+                .whereEqualTo("token", priceUpdate.getToken())  // Firestore query
+                .get();
+
+        // Process the notifications
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        List<Notification> notifications = documents.stream()
+                .map(doc -> doc.toObject(Notification.class))
+                .collect(Collectors.toList());
+
         // For each notification, check if the price condition is met
         for (Notification notification : notifications) {
             if (isThresholdReached(notification, priceUpdate.getPrice())) {
@@ -30,6 +45,7 @@ public class ThresholdCheckService {
         }
     }
 
+    // Check if the price condition has been reached
     public boolean isThresholdReached(Notification notification, Double currentPrice) {
         String type = notification.getNotificationType();
         double notificationValue = notification.getNotificationValue();
@@ -44,13 +60,14 @@ public class ThresholdCheckService {
         }
     }
 
-    // Directly notify the user from the notification entity
+    // Notify the user by sending the notification
     private void notifyUser(Notification notification) {
         NotificationDto notificationDto = new NotificationDto();
         notificationDto.setUserId(notification.getUserId());  // Use userId from the notification entity
         notificationDto.setRemarks("Price " + notification.getNotificationType() + " " +
                 notification.getNotificationValue() + " for token " + notification.getToken());
 
+        // Send the notification using the existing service
         notificationService.sendNotification(notificationDto);
     }
 }
