17f3e5352751e91d5fcbac6b265c5d11b0de996b
ngtianxun08 <e1327862@u.nus.edu>
2024-09-19 11:45:04 +0800

    * Bug fixes

--- src/test/java/com/webapp/notification/NotificationApplicationTests.java
+++ src/test/java/com/webapp/notification/NotificationApplicationTests.java
@@ -3,7 +3,7 @@ package com.webapp.notification;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.profiles.active=test")
+@SpringBootTest(properties = "spring.profiles.active=test", webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class NotificationApplicationTests {
 
 	@Test
