8df8504964e26aaaa81033aed847795734f355af
ngtianxun08 <e1327862@u.nus.edu>
2024-09-28 15:07:45 +0800

    * Readded logs.

--- src/main/java/com/webapp/notification/consumer/KafkaConsumerService.java
+++ src/main/java/com/webapp/notification/consumer/KafkaConsumerService.java
@@ -43,14 +43,15 @@ public class KafkaConsumerService {
             // Check if the price change is significant
             if (lastPrice == null || hasSignificantChange(lastPrice,
                     priceUpdateDto.getPrice())) {
-                logger.info("Price change for " + priceUpdateDto.getToken() + " is significant, processing.");
+                System.out.println("Price change for " + priceUpdateDto.getToken() + " is significant, processing.");
                 // Update the last price in the map
                 lastPriceMap.put(priceUpdateDto.getToken(), priceUpdateDto.getPrice());
 
                 // Process the price update
                 thresholdCheckService.checkThresholdsForAllUsers(priceUpdateDto);
             } else {
-                logger.info("Price change for " + priceUpdateDto.getToken() + " is insignificant, skipping.");
+                // System.out.println("Price change for " + priceUpdateDto.getToken() + " is
+                // insignificant, skipping.");
             }
         } catch (Exception e) {
             logger.error("Failed to deserialize message: " + message, e);
